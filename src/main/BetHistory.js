29c1b3b1b14199e3f79653728bae19297f2ec1e0
danny235 <danielbarima235@gmail.com>
2022-06-21 14:21:32 +0100

    correction

--- src/main/BetHistory.js
+++ src/main/BetHistory.js
@@ -165,7 +165,7 @@ const BetHistory = () => {
                     style={{ ...styles.cellStyle, color: colors.secondary }}
                     align="right"
                   >
-                    {row.outcome === "win" ? `+${row.profit}` : `-${row?.profit}`}
+                    {row.outcome === "win" ? `+${row.profit}` : "-"}
                   </TableCell>
                 </TableRow>
               ))}
